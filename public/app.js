9d56f75157453b7a251fa65285838e74c6b0b186
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2016-10-13 20:48:55 +0700

    add form and it can change value element

--- public/app.js
+++ public/app.js
@@ -1,25 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const Hello = React.createClass({
+// const Greeter = React.createClass({
 //   render() {
-//     return <h1>Hello World</h1>
+//     return <h1>Greeter World</h1>
 //   }
 // });
 
-class Hello extends React.Component {
+class Greeter extends React.Component {
+  constructor(props) {
+    super(props);
+    // initialize
+    this.onButtonClick = this.onButtonClick.bind(this);
+    this.state = {
+      name: this.props.name
+    }
+    console.log(this);
+  }
+  onButtonClick(e) {
+    e.preventDefault();
+    console.log(this.refs.name.value);
+    // alert(this.refs.name.value);
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+
+    if (typeof name === 'string' && name.length > 0) {
+      this.setState({
+        name
+      });
+      nameRef.value = '';
+    }
+  }
+
   render() {
     console.log(this);
-    return <h1>Hello {this.props.name}</h1>
+    var name = this.state.name;
+    var message = this.props.message;
+    return (
+      <div>
+        <h1>Hello {name}</h1>
+        <p>{message}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button type="submit">Set Name</button>
+        </form>
+      </div>
+    );
   }
 }
 
-Hello.propTypes = {
+Greeter.propTypes = {
   name: React.PropTypes.string.isRequired
 }
 
-Hello.defaultProps = {
-  name: 'React'
+Greeter.defaultProps = {
+  name: 'React',
+  message: 'This is from default props'
 }
 
-ReactDOM.render(<Hello/>, document.getElementById('app'));
+const firstName = 'Ikhsan';
+
+ReactDOM.render(<Greeter name={firstName}/>, document.getElementById('app'));
